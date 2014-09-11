72ad3cc3039b18ecdfd812bb0023ef3f11903680
Brent Komer <brent.komer@gmail.com>
2014-09-11 14:11:56 -0400

    adding odometry sensor for reference

--- kalman_filter/kalman_scenario.py
+++ kalman_filter/kalman_scenario.py
@@ -8,11 +8,16 @@ keyboard.properties( Speed=3.0 )
 robot.append( keyboard )
 
 pose = Pose()
-pose.frequency( frequency=30 )
+pose.frequency( frequency=10 )
 #pose.alter( 'Noise', pos_std = 0.3, rot_std=math.radians(10)) # Adding noise
 robot.append(pose)
 
+odom = Odometry()
+odom.frequency( frequency=10 )
+robot.append(odom)
+
 pose.add_interface( 'ros', topic='navbot/pose' )
+odom.add_interface( 'ros', topic='navbot/odometry' )
 
 env = Environment( 'outdoors' )
 env.place_camera( [ 10.0, -10.0, 10.0 ] )
